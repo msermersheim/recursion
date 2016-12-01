62d1e952ec9b93fe133ab8c09eb5057f68b81940
Michael Sermersheim <m.sermersheim@gmail.com>
2016-12-01 14:30:47 -0800

    ammending stringifyJSON to incorporate true recursion format

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,12 +3,21 @@
 
 // but you don't so you're going to write it from scratch:
 
+var iterateArray = function(obj, i){
+  //var arrayStringification = "";
+  if(i === 0){
+      return stringification += "[]";
+    }
+    stringifyJSON(obj[i]);
+    return iterateArray(obj, i-1); 
+};
+
+
 var stringifyJSON = function(obj) {
-  // your code goes here
   var stringification = "";
   if (typeof(obj) === "string"){
   	return stringification + '"'+obj+'"';
-  } else if(typeof(obj) === 'number'){
+  }else if(typeof(obj) === 'number'){
   	var num = obj;
   	return num.toString();
   }else if(obj === null){
@@ -18,91 +27,40 @@ var stringifyJSON = function(obj) {
   }else if(obj === true){
   	return "true";
   }else if (typeof(obj) === "object"){
-  	if((typeof obj.length === "number")===true)
-  	obj[i]
-
-  	return stringification +'['+obj.join(', ')+']';
-  }//else if (typeof(obj)==="object")
+  	if((Array.isArray(obj))===true){
+      iterateArray(obj, obj.length);  
+    }
+    //return 'object not array';
+  }
 };
 
-//if obj.length === number === true
-
-var iterateArrays = function(){
-  if (typeof(obj[i]) === "string"){
-    return stringification + '"'+obj+'"';
-  } else if(typeof(obj) === 'number'){
-    var num = obj;
-    return num.toString();
-  }else if(obj === null){
-    return "null";
-  }else if(obj === false){
-    return "false";
-  }else if(obj === true){
-    return "true";
-}
-
-var iterateForString = function(string){
-
-}
-
-var iterateArray = function(array, x, y){
-	if (y === array.length){
-		return;
-	}else if(x=== array[y].length){
-
-	}
-}
 
 
 
 
-// Capitalize function
-function stringify(element) {
-  
-  return .charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-}
 
-// Our recursive function
-function fixLetterCase(array, x, y) {
-  if (y === array.length) {
-    return;
-  }
-  else if (x === array[y].length) {
-    return fixLetterCase(array, 0, y + 1);
-  }
-  else {
-    array[y][x] = capitalize(array[y][x]);
-    return fixLetterCase(array, x + 1, y);
-  }	
-}
-
-fixLetterCase(capitals, 0, 0);
-
-console.log(capitals);
+    
 
 
-///////////
-// Our array of messy words
-var capitals = ["berlin", "parIs", "MaDRiD"];
 
-// Capitalize function
-function return(word) {
-  
-}
+/*
+else if(typeof(obj[i]) === "string"){
+      arrayStringification + '"'+obj[i]+'"';
+      return iterateArray(obj, i-1);
+    }else if(typeof(obj[i]) === 'number'){
+      var num = obj[i];
+      arrayStringification += num.toString();
+      return iterateArray(obj, i-1);
+    }else if(obj[i] === null){
+      arrayStringification += "null";
+      return iterateArray(obj, i-1);
+    }else if(obj[i] === false){
+      arrayStringification += "false";
+      return iterateArray(obj, i-1);
+    }else if(obj[i] === true){
+      arrayStringification += "true";
+      return iterateArray(obj, i-1);
+    }
+*/
 
-// Our recursive function
-function fixLetterCase(array, i) {    
-  // Base case
-  if (i === array.length) {
-    return;
-  } 
-  // Action
-  array[i] = capitalize(array[i]);
-  // Recursive case
-  return fixLetterCase(array, i + 1);
-}
 
-// Here is our function call
-fixLetterCase(capitals, 0);
-
-console.log(capitals);
